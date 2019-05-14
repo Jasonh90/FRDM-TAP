3e19a5e47c06c456e348b2135e4a17260f8931d8
Jason <jh2424@cornell.edu>
2019-05-13 23:05:57 -0400

    s

--- main.c
+++ main.c
@@ -2,41 +2,18 @@
 #include "SSD1306.h"
 #include "milliseconds.h"
 
-int main() {
-  setup_timer();
-  SSD1306_begin();
 
-  delay(1000);
-	//SSD1306_hello();
-
-    /* Push-button test */
-    led_setup();
-    GPIO_setup();
-    LEDRed_On();
-    
-    while(1) {
-        if (PTC->PDIR == (0u << 3)) {             // Switch is pressed
-            LEDRed_Toggle();
-        }
-    }
-    
-	
-	SSD1306_play();
-	
-	//Scroll_Setup(1, 0, 1, 7); 
-	return 0;
-}
 
 
 void GPIO_setup(void) {
     SIM->SCGC5 = SIM_SCGC5_PORTC_MASK;        // enable clock to port C
     PORTC->PCR[12] = PORT_PCR_MUX(001);      // enable PTC12 as GPIO
-    PTC->PDDR = (0u << 3)                   // enable PTC12 as INPUT
+    PTC->PDDR = (0u << 3);                   // enable PTC12 as INPUT
 }
 
 
 /* From lab 2 */
-}
+
 void led_setup(void){
     SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;           //enable the clock to port B
     PORTB->PCR[22] = PORT_PCR_MUX(001);          // set up PTB22 as GPIO
@@ -74,3 +51,27 @@ void LEDRed_On (void) {
 //    PTD->PDDR=(1u<<5);//PIN 5 of portd as OUTPUT
 //
 //}
+int main() {
+  setup_timer();
+  SSD1306_begin();
+
+  delay(1000);
+	//SSD1306_hello();
+
+    /* Push-button test */
+    led_setup();
+    GPIO_setup();
+    LEDRed_On();
+    
+    while(1) {
+        if (PTC->PDIR == (0u << 3)) {             // Switch is pressed
+            LEDRed_Toggle();
+        }
+    }
+    
+	
+	SSD1306_play();
+	
+	//Scroll_Setup(1, 0, 1, 7); 
+	return 0;
+}
